0d62ae9c4facc90022116946a77125faa7af2731
sanjanamoghe <sanjana.moghe@gmail.com>
2022-08-23 17:39:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('Welcome to the Healthy Diner!')
 streamlit.header('Menu')
-streamlit.text('Avocado smoothie')
+streamlit.text('🥑 Avocado smoothie')
 streamlit.text('🐔 Chicken salad')
-streamlit.text('Egg White Omlette')
+streamlit.text('🐔 Egg White Omlette')
 streamlit.text('🥗 Caesar salad')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
