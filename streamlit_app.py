21773f1fdebb3f73e2a1add0d6ad48f08b3d76fa
sanjanamoghe <sanjana.moghe@gmail.com>
2022-08-23 14:59:14 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Welcome to the Healthy Diner!')
+streamlit.header('Menu')
+streamlit.text('Avocado smoothie')
+streamlit.text('Paneer Pulao')
+streamlit.text('Egg White Omlette')
+streamlit.text('Caesar salad')
