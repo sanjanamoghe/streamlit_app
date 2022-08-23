d1b2aad109bd918c0b276d72f22abb0812987a33
sanjanamoghe <sanjana.moghe@gmail.com>
2022-08-23 17:41:58 +0530

    Update streamlit_app.py
    reading csv from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Welcome to the Healthy Diner!')
 streamlit.header('Menu')
@@ -9,3 +10,5 @@ streamlit.text('🥗 Caesar salad')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
